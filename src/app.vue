db3b044f9012ccb4a38ae16e1557e9d20dba47ae
teracy164 <literacy1945@gmail.com>
2024-07-23 18:54:36 +0900

    問い合わせ先を追加

--- src/app.vue
+++ src/app.vue
@@ -3,6 +3,10 @@
     <Header />
     <main>
       <NuxtPage />
+      <div style="text-align: center; margin: 2em 0; font-size: 0.7em">
+        <el-link type="primary" size="small" href="https://x.com/teracy164" target="_blank"> 問い合わせはX（旧Twitter）まで </el-link>
+        <div>Created by teracy</div>
+      </div>
     </main>
   </div>
 </template>
