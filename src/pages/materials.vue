9e6ec764866ae18aafb332ca7b0aafe07ca3cc6b
teracy164 <literacy1945@gmail.com>
2024-08-11 12:28:14 +0900

    育成計算機のページにuseSeoMetaを追加

--- src/pages/materials.vue
+++ src/pages/materials.vue
@@ -4,8 +4,17 @@
     <MaterialList />
   </div>
 </template>
-<script lang="ts"></script>
-<style lang="scss">
-.materials {
-}
-</style>
+<script lang="ts">
+const description = `ゼンレスゾーンゼロ用の育成計算機です。
+キャラのLvやスキルのLv、音動機のLvを指定することで、育成に必要な素材をまとめて計算します。
+キャラや音動機の素材一覧もこちらで確認可能です。
+`;
+
+useSeoMeta({
+  title: 'ゼンレスゾーンゼロ　育成計算機',
+  ogTitle: 'ゼンレスゾーンゼロ　育成計算機',
+  description,
+  ogDescription: description,
+});
+</script>
+<style lang="scss"></style>
