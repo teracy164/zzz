63e267a03b59a295085e1d8e0ac15dfb968872a3
teracy164 <literacy1945@gmail.com>
2025-01-09 21:23:23 +0900

    AD対応修正

--- src/app.vue
+++ src/app.vue
@@ -29,9 +29,28 @@ useSeoMeta({
   ogDescription: description,
 });
 onMounted(() => {
-  const elScript = document.createElement('script');
-  elScript.type = 'text/javascript';
-  elScript.src = 'https://adm.shinobi.jp/s/518c8ffee39c2c2a3a4c44d04fbeb9f6';
-  document.getElementById('admax-ad-container').appendChild(elScript);
+  const iframe = document.createElement('iframe');
+  document.getElementById('admax-ad-container').appendChild(iframe);
+
+  const html = '<body><script src="https://adm.shinobi.jp/s/518c8ffee39c2c2a3a4c44d04fbeb9f6"><\/script><\/body>';
+  const iframeDocument = iframe.contentWindow.document;
+  iframeDocument.open();
+  iframeDocument.write(html);
+  iframeDocument.close();
+
+  // const elScript = document.createElement('script');
+  // elScript.type = 'text/javascript';
+  // elScript.src = 'https://adm.shinobi.jp/s/518c8ffee39c2c2a3a4c44d04fbeb9f6';
+  // elScript.async = true;
+
+  // elScript.onload = () => {
+  //   (globalThis.admaxads = (window as any).admaxads || []).push({ admax_id: '518c8ffee39c2c2a3a4c44d04fbeb9f6', type: 'action' });
+  //   console.log(globalThis.admaxads);
+  // };
+  // elScript.onerror = () => {
+  //   console.error('failed load ad js.');
+  // };
+
+  // document.getElementById('admax-ad-container').appendChild(elScript);
 });
 </script>
