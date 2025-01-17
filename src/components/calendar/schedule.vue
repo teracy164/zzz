a1cc6b7af44b562a1041353de8a284d935ef0449
teracy164 <literacy1945@gmail.com>
2025-01-17 21:41:36 +0900

    カレンダーの各種サイズを調整

--- src/components/calendar/schedule.vue
+++ src/components/calendar/schedule.vue
@@ -17,8 +17,20 @@ const calendarOptions = {
 </script>
 <style lang="scss">
 .schedule-calendar {
+  .fc-header-toolbar {
+    margin-bottom: 0.25em !important;
+    .fc-toolbar-title {
+      font-size: 1.25em !important;
+    }
+    .fc-button {
+      padding: 0 0.25em !important;
+    }
+  }
   .fc-day-today {
     background-color: rgb(82, 82, 39) !important;
+  }
+  .fc-daygrid-day-number {
+    font-size: 0.8em !important;
   }
 }
 </style>
