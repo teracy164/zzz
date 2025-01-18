05d0ea1e831244c1750adb514460ac2a272780f1
teracy164 <literacy1945@gmail.com>
2025-01-19 08:56:50 +0900

    アストラ育成素材について追加

---
+++ src/pages/news/agent/20250119-astra.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="flex justify-center">
+    <Card>
+      <h1 class="bg-red-500 p-2">
+        <label>【エージェント情報】アストラ育成素材判明！！　※非リーク</label>
+      </h1>
+
+      <div class="flex justify-end my-2">
+        <ShareBtnTwitter title="【エージェント情報】アストラ育成素材判明！！　※非リーク" :page-url="getPageUrl()" />
+      </div>
+
+      <div class="mb-5">
+        1月22日（水）に実装予定のアストラの育成素材が公開されました！
+        <br />
+        ※HoYoLabの育成ガイドによる情報のため、リークではありません
+
+        <div class="m-2">
+          <img
+            src="https://upload-os-bbs.hoyolab.com/upload/2025/01/18/179183172/dd6a3a947ce04a87ee5895446009aa18_1571353644964055886.png?x-oss-process=image%2Fresize%2Cs_1000%2Fauto-orient%2C0%2Finterlace%2C1%2Fformat%2Cwebp%2Fquality%2Cq_70"
+            style="object-fit: contain"
+          />
+        </div>
+      </div>
+
+      <div class="flex justify-center">
+        <div class="m-1">
+          <a href="https://px.a8.net/svt/ejp?a8mat=44WWAW+2QTZ76+NA2+5Z6WX" rel="nofollow">
+            <img
+              border="0"
+              width="125"
+              height="125"
+              alt=""
+              src="https://www21.a8.net/svt/bgt?aid=250118024166&wid=001&eno=01&mid=s00000003017001004000&mc=1"
+          /></a>
+          <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=44WWAW+2QTZ76+NA2+5Z6WX" alt="" />
+        </div>
+        <div class="m-1">
+          <a href="https://px.a8.net/svt/ejp?a8mat=44WWAY+DKSWFM+2ULW+6GZCH" rel="nofollow">
+            <img
+              border="0"
+              width="125"
+              height="125"
+              alt=""
+              src="https://www21.a8.net/svt/bgt?aid=250118026821&wid=001&eno=01&mid=s00000013298001087000&mc=1"
+          /></a>
+          <img border="0" width="1" height="1" src="https://www19.a8.net/0.gif?a8mat=44WWAY+DKSWFM+2ULW+6GZCH" alt="" />
+        </div>
+        <div class="m-1">
+          <a href="https://px.a8.net/svt/ejp?a8mat=44WWAY+EPADKI+24JA+6FP1T" rel="nofollow">
+            <img
+              border="0"
+              width="125"
+              height="125"
+              alt=""
+              src="https://www21.a8.net/svt/bgt?aid=250118026889&wid=001&eno=01&mid=s00000009919001081000&mc=1"
+          /></a>
+          <img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=44WWAY+EPADKI+24JA+6FP1T" alt="" />
+        </div>
+      </div>
+
+      <div>
+        <h3>
+          <label class="bg-yellow-500">要警戒狩り</label>
+        </h3>
+        <div class="pl-5">
+          <p>必要な素材は「終幕のダンスシューズ」です</p>
+          <div class="flex flex-col md:flex-row mb-2">
+            <div>
+              <img
+                alt="終幕のダンスシューズ"
+                src="/images/materials/notorious-hunt/marionette.png"
+                style="max-width: 300px; max-height: 300px; object-fit: contain"
+                class="mx-2"
+              />
+            </div>
+
+            <div>
+              <label class="bg-blue-400 px-2" style="border-radius: 2px">【必要数】</label>
+              <div class="mx-2">
+                <div class="flex"><label style="width: 4.5em">Aのみ：</label>0</div>
+                <div class="flex"><label style="width: 4.5em">A～B：</label>0</div>
+                <div class="flex"><label style="width: 4.5em">A～C：</label>0</div>
+                <div class="flex"><label style="width: 4.5em">A～D：</label>2</div>
+                <div class="flex"><label style="width: 4.5em">A～E：</label>5</div>
+                <div class="flex"><label style="width: 4.5em">A～F：</label>9</div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p>「マリオネット・ツインズ」から取得できます</p>
+            <img
+              alt="マリオネット・ツインズ"
+              src="/images/enemies/notorious-hunt/marionette.png"
+              style="max-width: 300px; max-height: 200px; object-fit: contain"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-5">
+        <h3>
+          <label class="bg-purple-500">エキスパート挑戦</label>
+        </h3>
+        <div class="pl-5">
+          <p>必要な素材は「高次元データ：閃く閃光」です</p>
+          <div class="flex flex-col md:flex-row mb-2">
+            <div>
+              <img
+                alt="高次元データ：閃く閃光"
+                src="/images/materials/expert-challenge/thracian.png"
+                style="max-width: 300px; max-height: 300px; object-fit: contain"
+              />
+            </div>
+
+            <div>
+              <div class="mx-2">
+                <label class="bg-blue-400 px-2" style="border-radius: 2px">【必要数】</label>
+                <div class="flex"><label style="width: 4.5em">Aのみ：</label>0</div>
+                <div class="flex"><label style="width: 4.5em">A～B：</label>2</div>
+                <div class="flex"><label style="width: 4.5em">A～C：</label>6</div>
+                <div class="flex"><label style="width: 4.5em">A～D：</label>15</div>
+                <div class="flex"><label style="width: 4.5em">A～E：</label>30</div>
+                <div class="flex"><label style="width: 4.5em">A～F：</label>60</div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p>「トラキアン」から取得できます</p>
+            <img
+              alt="トラキアン"
+              src="/images/enemies/expert-challenge/thracian.png"
+              style="max-width: 300px; max-height: 200px; object-fit: contain"
+            />
+          </div>
+        </div>
+      </div>
+    </Card>
+  </div>
+</template>
+<script lang="ts" setup>
+const description = ['ゼンレスゾーンゼロのエージェント「アストラ」の育成素材が判明！！', '※公式ツールによる情報'].join('\n');
+useSeoMeta({
+  title: '【エージェント情報】アストラ育成素材判明！！　※非リーク',
+  ogTitle: '【エージェント情報】アストラ育成素材判明！！　※非リーク',
+  description,
+  ogDescription: description,
+});
+
+const getPageUrl = () => {
+  return location.href;
+};
+</script>
+<style lang="scss" scoped>
+table.item-info {
+  th,
+  td {
+    border: 1px solid lightgray;
+    padding: 0.25em;
+  }
+  th {
+    min-width: 5em;
+    text-align: left;
+  }
+}
+</style>
--- src/shared/data/news.ts
+++ src/shared/data/news.ts
@@ -2,5 +2,6 @@ import type { NewsItem } from '~/types/news';
 
 /** ニュースアイテム（追加するときはcreatedAt降順となるように追加する） */
 export const NEWS: NewsItem[] = [
+  { createdAt: '2025/01/19', title: '【エージェント情報】アストラ育成素材判明！！　※非リーク', link: '/news/agent/20250119-astra' },
   { createdAt: '2025/01/18', title: '【グッズ情報】エレン・ジョー 月曜から金曜まで【フィギュア】', link: '/news/goods/20250118' },
 ];
