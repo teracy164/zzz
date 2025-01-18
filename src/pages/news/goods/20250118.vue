a4ba0c06061a05541880309129045132473b9d96
teracy164 <literacy1945@gmail.com>
2025-01-18 12:11:28 +0900

    グッズ情報修正

--- src/pages/news/goods/20250118.vue
+++ src/pages/news/goods/20250118.vue
@@ -1,10 +1,32 @@
 <template>
   <div class="flex justify-center">
     <Card>
-      <h1>【あみあみ】エレン・ジョー 月曜から金曜までVer. 1/8　フィギュア予約</h1>
+      <h1 class="bg-red-500 p-2">
+        <label>【グッズ情報】エレン・ジョー 月曜から金曜までVer. 1/8　フィギュア予約</label>
+      </h1>
 
       <div class="my-5">
-        あみあみにて、Ver1.5で追加されるエレンの衣装「月曜から金曜まで」のフィギュアの予約が開始されました 2025年9月に発売予定です
+        Ver1.5で追加されるエレンの衣装「月曜から金曜まで」のフィギュアの予約が開始されました!
+        <br />
+        発売日は2025年9月を予定しています
+
+        <div class="my-5 w-full flex flex-col justify-center items-center">
+          <div style="width: 600px; max-width: 70vw">
+            <a
+              href="https://px.a8.net/svt/ejp?a8mat=44WWAW+2QTZ76+NA2+BWGDT&a8ejpredirect=https%3A%2F%2Fwww.amiami.jp%2Ftop%2Fdetail%2Fdetail%3Fgcode%3DFIGURE-181127%26_gl%3D1%252a166ljwo%252a_ga%252aMTMwNDM0MDIyNy4xNzMwMzc4MDQx%252a_ga_DNC11S3TQ3%252aMTczNzE1MzM2OC4xMi4xLjE3MzcxNTUyNDkuMC4wLjA.%26%3Faffid%3Dfc"
+              rel="nofollow"
+              alt=""
+            >
+              <img border="0" alt="" src="https://img.amiami.jp/images/product/review/251/FIGURE-181127_06.jpg"
+            /></a>
+            <img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=44WWAW+2QTZ76+NA2+BWGDT" alt="" />
+          </div>
+          <p>© Copyright (C) miHoYo. All Rights Reserved</p>
+        </div>
+
+        <p class="mt-5">
+          エレンらしいめんどくさそうな表情や、立派なサメのシッポが再現されて、1万円以下とは思えないクオリティとなっています。
+        </p>
 
         <div class="flex justify-center mt-5">
           <a href="https://px.a8.net/svt/ejp?a8mat=44WWAW+2QTZ76+NA2+656YP" rel="nofollow">
@@ -18,48 +40,67 @@
           <img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=44WWAW+2QTZ76+NA2+656YP" alt="" />
         </div>
 
-        <p class="mt-5">
-          エレンらしいめんどくさそうな表情や、立派なサメのシッポが再現されて、1万円以下とは思えないクオリティとなっています。
-        </p>
+        <h3 class="mt-5"><label class="bg-red-400 px-2">商品情報</label></h3>
+        <table class="item-info">
+          <tbody>
+            <tr>
+              <th>発売日</th>
+              <td>2025年9月</td>
+            </tr>
+            <tr>
+              <th>価格</th>
+              <td>
+                6,600円(税込)
+                <p>あみあみでは2%OFFの6,460円(税込)　※1/18時点</p>
+              </td>
+            </tr>
+            <tr>
+              <th>製品仕様</th>
+              <td>スケール：1/8、高さ：約15.2cm(台座含め) 、素材：pVC＆ABS</td>
+            </tr>
+            <tr>
+              <th>商品ページ</th>
+              <td>
+                <el-link type="primary">
+                  <a
+                    rel="nofollow"
+                    target="_blank"
+                    href="https://px.a8.net/svt/ejp?a8mat=44WWAW+2QTZ76+NA2+BWGDT&a8ejpredirect=https%3A%2F%2Fwww.amiami.jp%2Ftop%2Fdetail%2Fdetail%3Fgcode%3DFIGURE-181127%26_gl%3D1%252a166ljwo%252a_ga%252aMTMwNDM0MDIyNy4xNzMwMzc4MDQx%252a_ga_DNC11S3TQ3%252aMTczNzE1MzM2OC4xMi4xLjE3MzcxNTUyNDkuMC4wLjA.%26%3Faffid%3Dfc"
+                    >あみあみ | LIMEPIEシリーズ ゼンレスゾーンゼロ エレン・ジョー 月曜から金曜までVer. 1/8
+                    完成品フィギュア[APEX]《０９月予約》</a
+                  >
+                </el-link>
+                <p>※もしくは以下の画像クリックでも表示できます</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-        <el-link type="primary" class="mt-5">
+      <div class="my-5 w-full flex flex-col justify-center items-center">
+        <div style="width: 600px; max-width: 70vw">
           <a
-            rel="nofollow"
-            target="_blank"
             href="https://px.a8.net/svt/ejp?a8mat=44WWAW+2QTZ76+NA2+BWGDT&a8ejpredirect=https%3A%2F%2Fwww.amiami.jp%2Ftop%2Fdetail%2Fdetail%3Fgcode%3DFIGURE-181127%26_gl%3D1%252a166ljwo%252a_ga%252aMTMwNDM0MDIyNy4xNzMwMzc4MDQx%252a_ga_DNC11S3TQ3%252aMTczNzE1MzM2OC4xMi4xLjE3MzcxNTUyNDkuMC4wLjA.%26%3Faffid%3Dfc"
-            >商品のページはこちらから</a
+            rel="nofollow"
           >
-        </el-link>
-        <p>※もしくは以下の画像クリックでも表示できます</p>
+            <img border="0" alt="" src="https://img.amiami.jp/images/product/review/251/FIGURE-181127_01.jpg"
+          /></a>
+          <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=44WWAW+2QTZ76+NA2+BWGDT" alt="" />
+        </div>
+        <p>© Copyright (C) miHoYo. All Rights Reserved</p>
       </div>
 
-      <div class="flex justify-center">
-        <a
-          href="https://px.a8.net/svt/ejp?a8mat=44WWAW+2QTZ76+NA2+BWGDT&a8ejpredirect=https%3A%2F%2Fwww.amiami.jp%2Ftop%2Fdetail%2Fdetail%3Fgcode%3DFIGURE-181127%26_gl%3D1%252a166ljwo%252a_ga%252aMTMwNDM0MDIyNy4xNzMwMzc4MDQx%252a_ga_DNC11S3TQ3%252aMTczNzE1MzM2OC4xMi4xLjE3MzcxNTUyNDkuMC4wLjA.%26%3Faffid%3Dfc"
-          rel="nofollow"
-          alt=""
-        >
-          <img border="0" alt="" src="https://img.amiami.jp/images/product/review/251/FIGURE-181127_06.jpg"
-        /></a>
-        <img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=44WWAW+2QTZ76+NA2+BWGDT" alt="" />
-      </div>
-      <div class="flex justify-center">
-        <a
-          href="https://px.a8.net/svt/ejp?a8mat=44WWAW+2QTZ76+NA2+BWGDT&a8ejpredirect=https%3A%2F%2Fwww.amiami.jp%2Ftop%2Fdetail%2Fdetail%3Fgcode%3DFIGURE-181127%26_gl%3D1%252a166ljwo%252a_ga%252aMTMwNDM0MDIyNy4xNzMwMzc4MDQx%252a_ga_DNC11S3TQ3%252aMTczNzE1MzM2OC4xMi4xLjE3MzcxNTUyNDkuMC4wLjA.%26%3Faffid%3Dfc"
-          rel="nofollow"
-        >
-          <img border="0" alt="" src="https://img.amiami.jp/images/product/review/251/FIGURE-181127_01.jpg"
-        /></a>
-        <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=44WWAW+2QTZ76+NA2+BWGDT" alt="" />
-      </div>
-      <div class="flex justify-center">
-        <a
-          href="https://px.a8.net/svt/ejp?a8mat=44WWAW+2QTZ76+NA2+BWGDT&a8ejpredirect=https%3A%2F%2Fwww.amiami.jp%2Ftop%2Fdetail%2Fdetail%3Fgcode%3DFIGURE-181127%26_gl%3D1%252a166ljwo%252a_ga%252aMTMwNDM0MDIyNy4xNzMwMzc4MDQx%252a_ga_DNC11S3TQ3%252aMTczNzE1MzM2OC4xMi4xLjE3MzcxNTUyNDkuMC4wLjA.%26%3Faffid%3Dfc"
-          rel="nofollow"
-        >
-          <img border="0" alt="" src="https://img.amiami.jp/images/product/main/251/FIGURE-181127.jpg"
-        /></a>
-        <img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=44WWAW+2QTZ76+NA2+BWGDT" alt="" />
+      <div class="my-5 w-full flex flex-col justify-center items-center">
+        <div style="width: 600px; max-width: 70vw">
+          <a
+            href="https://px.a8.net/svt/ejp?a8mat=44WWAW+2QTZ76+NA2+BWGDT&a8ejpredirect=https%3A%2F%2Fwww.amiami.jp%2Ftop%2Fdetail%2Fdetail%3Fgcode%3DFIGURE-181127%26_gl%3D1%252a166ljwo%252a_ga%252aMTMwNDM0MDIyNy4xNzMwMzc4MDQx%252a_ga_DNC11S3TQ3%252aMTczNzE1MzM2OC4xMi4xLjE3MzcxNTUyNDkuMC4wLjA.%26%3Faffid%3Dfc"
+            rel="nofollow"
+          >
+            <img border="0" alt="" src="https://img.amiami.jp/images/product/main/251/FIGURE-181127.jpg"
+          /></a>
+          <img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=44WWAW+2QTZ76+NA2+BWGDT" alt="" />
+        </div>
+        <p>© Copyright (C) miHoYo. All Rights Reserved</p>
       </div>
     </Card>
   </div>
@@ -76,3 +117,16 @@ useSeoMeta({
   ogDescription: description,
 });
 </script>
+<style lang="scss" scoped>
+table.item-info {
+  th,
+  td {
+    border: 1px solid lightgray;
+    padding: 0.25em;
+  }
+  th {
+    min-width: 5em;
+    text-align: left;
+  }
+}
+</style>
