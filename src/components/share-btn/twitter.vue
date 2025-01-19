3fb9754cb4ccb4ca90fe80e2b4d5555d1df7d03d
teracy164 <literacy1945@gmail.com>
2025-01-19 10:14:11 +0900

    Xシェアボタン修正

--- src/components/share-btn/twitter.vue
+++ src/components/share-btn/twitter.vue
@@ -1,5 +1,5 @@
 <template>
-  <a :href="`https://twitter.com/intent/tweet?text=${title}&url=${pageUrl}`" target="_blank" rel="noopener noreferrer">
+  <a :href="`https://twitter.com/intent/tweet?text=${message}`" target="_blank" rel="noopener noreferrer">
     <el-button>
       <img src="/images/x-logo-white.png" style="width: 1em; height: 1em" />
       <span class="px-2">シェア</span>
@@ -8,6 +8,9 @@
 </template>
 <script lang="ts" setup>
 const { title, pageUrl } = defineProps<{ title: string; pageUrl: string }>();
+
+const hashTag = ['ゼンレスゾーンゼロ', 'ゼンゼロ'].map((tag) => `%23${tag}`).join(' ');
+const message = encodeURI(`${title}\n${pageUrl}\n`) + hashTag;
 </script>
 <style lang="scss" scoped>
 button {
