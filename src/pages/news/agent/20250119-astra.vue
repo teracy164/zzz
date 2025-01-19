ba40c8a7921ff89343397ae1e024801a13c59ac2
teracy164 <literacy1945@gmail.com>
2025-01-19 12:22:57 +0900

    画像幅調整

--- src/pages/news/agent/20250119-astra.vue
+++ src/pages/news/agent/20250119-astra.vue
@@ -17,7 +17,7 @@
         <div class="m-2">
           <img
             src="https://upload-os-bbs.hoyolab.com/upload/2025/01/18/179183172/dd6a3a947ce04a87ee5895446009aa18_1571353644964055886.png?x-oss-process=image%2Fresize%2Cs_1000%2Fauto-orient%2C0%2Finterlace%2C1%2Fformat%2Cwebp%2Fquality%2Cq_70"
-            style="object-fit: contain"
+            style="width: 600px; object-fit: contain"
           />
         </div>
       </div>
